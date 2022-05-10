cf10ac41cdfed8e563a65dc224a4616925710557
Sergey Lukin <contact@sergeylukin.com>
2022-05-10 06:29:57 +0300

    feat: lock versions by default

--- npmlock.sh
+++ npmlock.sh
@@ -13,7 +13,7 @@ npmlock () {
   local found paths seen
   declare -a paths=()
   declare -a seen=()
-  declare -a found=()
+  declare -a found=("asd")
   local package="$1"
 
   case "$package" in
@@ -36,16 +36,35 @@ npmlock () {
   esac
 
   ## business logic
+  {
+    newjs=$(cat package.json)
+    while read -r pkg; read -r version; do
+          trimmed_pkg_name=$(echo $pkg | tr -d '"')
+          resolved=$(cat package-lock.json | jq --arg pkg "$trimmed_pkg_name" '.dependencies[$pkg].version')
+          trimmed_pkg_version=$(echo $resolved | tr -d '"')
+          echo "$pkg $version -> $trimmed_pkg_version";
+          newjs=$(echo $newjs | jq --arg pkg "$trimmed_pkg_name" --arg version "$trimmed_pkg_version" '.dependencies[$pkg] = $version')
+          echo $newjs > /tmp/new.json
+          jq '.' /tmp/new.json > /tmp/new2.json
+          ## add to found count
+          found+=("asd")
+        done < <(cat package.json | jq -r '.dependencies | to_entries
+                  | map_values(
+                  (.value) as $v
+                  | { name: .key, version: $v })[] | .name, .version')
 
-  ## get total
-  count="${#found[@]}"
+        mv /tmp/new2.json package.json
+        rm -f /tmp/new.json /tmp/new2.json
+      }
 
-  if (( count == 1 )); then
+  count=${#found[@]}
+
+  if [ $count -eq 1 ]; then
     echo "${found[0]}"
-  elif (( count > 0 )); then
-    printf "npmlock: found %d result(s)\n" "$count"
-  else
-    echo "npmlock: Couldn't find anything that matches \`$package'"
+  elif (( $count > 0 )); then
+    echo "found $count results"
+  elif [[ ! -z "$package" ]]; then
+    echo "npmlock: Couldn't find anything that matches '$package'"
     return 1
   fi
   return 0
